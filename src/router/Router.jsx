9c9cf3fa554bd5b98d84c34228a862db4ba494e4
BeatrizRodriguezReglero <beatriz.rreglero@gmail.com>
2024-06-13 21:28:15 +0200

    changes

---
+++ src/router/Router.jsx
@@ -0,0 +1,15 @@
+
+import {Routes, Route} from "react-dom"
+const Router =()=>{
+    return(
+
+        <>
+            <Routes>
+
+                <Route/>
+            </Routes>
+        </>
+    )
+
+} 
+export default Router
